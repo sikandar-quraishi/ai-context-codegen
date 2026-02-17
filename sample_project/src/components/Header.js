2f86572ea1a62f1758a47b075e83045230225948
Sikandar Quraishi <squraishi8846@gmail.com>
2026-02-17 17:21:51 +0530

    Delete sample_project/src directory

--- sample_project/src/components/Header.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Header() {
-  return (
-    <header style={{background: "#282c34", padding: "20px", color: "white"}}>
-      <h1>Sample App Header</h1>
-    </header>
-  );
-}
